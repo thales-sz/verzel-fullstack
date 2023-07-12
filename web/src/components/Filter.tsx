4be85aa08e80197f35b12d3095d6f2efd6d96930
Thales <thales.souz@outlook.com>
2023-07-12 13:53:26 -0300

    feat: filter component

--- web/src/components/Filter.tsx
+++ web/src/components/Filter.tsx
@@ -1,13 +1,16 @@
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 function Filter() {
   return (
     <aside className='w-4/5 self-stretch text-center'>
       <div className="flex flex-col gap-4">
         <h1 className='font-bold text-2xl'>Ordernar por:</h1>
         <div className="relative w-full lg:max-w-sm">
-            <select className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
-                <option selected hidden>
+            <select className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm focus:border-indigo-600">
+                <option selected className="hidden">
                   Filtro
-                  <i className="fa-solid fa-user"></i>
+                  <FontAwesomeIcon icon={faAngleDown} />
                 </option>
                 <option>Maior preço</option>
                 <option>Menor preço</option>
