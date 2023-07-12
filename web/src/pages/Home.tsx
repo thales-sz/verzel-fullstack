9823ee984aa5e46e4b8ebdc25ce219c5c8ed750b
Thales <thales.souz@outlook.com>
2023-07-12 10:14:13 -0300

    feat: mock data from kavak itens

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,10 +1,27 @@
+import Loading from '../components/Loading';
 import '../styles/main.css'
+import { Car, getData } from '../../../crawler/src/appCrawler';
+import { useQuery } from 'react-query';
 
 function Home() {
+
+  const { isLoading, data } = useQuery({
+    queryFn: getData,
+    refetchOnWindowFocus: false
+  })
+
   return (
-    <div>
-      Home Page
-    </div>
+    <section>
+      {isLoading ? <Loading /> : data?.map((item: Car, index) => (
+        <div key={index}>
+          <h1>{item.name}</h1>
+          <p>{item.name.split(' ')[0]}</p>
+          <p>{item.price}</p>
+          <img src={item.image} alt={item.name} />
+        </div>
+      )
+      )}
+    </section>
   )
 }
 
